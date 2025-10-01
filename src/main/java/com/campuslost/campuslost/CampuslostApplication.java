d480c3dd8efdc59067a5c30068330daa2503d871
Your Name <you@example.com>
2025-10-01 11:49:12 +0530

    Initial commit – Spring Boot skeleton

---
+++ src/main/java/com/campuslost/campuslost/CampuslostApplication.java
@@ -0,0 +1,13 @@
+package com.campuslost.campuslost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuslostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuslostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campuslost/campuslost/CampuslostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuslost.campuslost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuslostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
